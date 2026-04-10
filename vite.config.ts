f99103528765592464038621606c4258289cf6e2
Elise Halmingh <elise.halmingh@gmail.com>
2026-04-11 00:32:48 +0200

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
     hmr: {
       overlay: false,
     },
+    base: '/yoga-site/',
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
